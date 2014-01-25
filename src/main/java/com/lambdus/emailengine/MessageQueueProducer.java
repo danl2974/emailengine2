3fada16f43aebcabf33e9919a2bb49dd04bbe3fa
Dan <danl2974@msn.com>
2014-01-25 00:46:14 -0500

    Jan 25 12:45

--- src/main/java/com/lambdus/emailengine/MessageQueueProducer.java
+++ src/main/java/com/lambdus/emailengine/MessageQueueProducer.java
@@ -1,6 +1,8 @@
 package com.lambdus.emailengine;
 
 import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
@@ -9,19 +11,27 @@ import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.naming.InitialContext;
+
+import org.jboss.logging.Logger;
+
+import com.lambdus.emailengine.webservices.RestDefinition;
 
 
-
+@Stateless
+@LocalBean
 public class MessageQueueProducer {
-	
-    @Resource(mappedName = "java:/ConnectionFactory")
+        
+    private static final Logger log = Logger.getLogger(MessageQueueProducer.class.getName());
+        
+    //@Resource(name = "java:/ConnectionFactory")
     private ConnectionFactory connectionFactory;
 
-    @Resource(mappedName = "java:/queue/transactionalemail")
+    //@Resource(name = "java:/queue/transactionalemail")
     private Queue queueTx;
     
     private EmailRequest request;
-    private EmailMessage emailMessage;
+    private EmailMessage emailMessage = new EmailMessage();
     
     private MessageProducer messageProducer;
     private Connection connection = null;
@@ -29,63 +39,104 @@ public class MessageQueueProducer {
     private Destination destination;
     private MapMessage mapMessage;
     
+    
+    /*
     public MessageQueueProducer(EmailRequest request) {
-    	this.request = request;
+            log.info(request.getTemplateId());
+            this.request = request;
+    }
+    */
+    
+    public void addRequest(EmailRequest request){
+            
+            log.info(request.getTemplateId());
+            this.request = request;
+            
     }
     
     public void initialize(){
-    	// resolve the message assembly
-    	MessageAssembler ma = new MessageAssembler(this.request.getTemplateId(), this.request.getParameters());
-    	
-    	this.emailMessage.emailCreative = ma.getAssembledMessage();
-    	this.emailMessage.emailAddress = this.request.getEmailAddress();
-    	this.emailMessage.subjectLine = ma.getSubjectLine();
-    	this.emailMessage.fromAddress = ma.getFromAddress();
+            // resolve the message assembly
+            MessageAssembler ma = new MessageAssembler(this.request.getTemplateId(), this.request.getParameters());
+            
+            this.emailMessage.emailCreative = ma.getAssembledMessage();
+            this.emailMessage.emailAddress = this.request.getEmailAddress();
+            this.emailMessage.subjectLine = ma.getSubjectLine();
+            this.emailMessage.fromAddress = ma.getFromAddress();
+            
+            startConnection();
+            createMessage();
+            sendMessage();
+            
+            
     }
  
     public void startConnection()
     {
-    	try
-    	{	
+          log.info("jms startConnection called");
+          try
+          {
+          InitialContext ctx = new InitialContext();
+          connectionFactory = (ConnectionFactory) ctx.lookup("java:/ConnectionFactory");
+          queueTx = (Queue) ctx.lookup("java:/queue/transactionalemail");
           this.connection = this.connectionFactory.createConnection();
+          log.info("after connection");
           this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-          this.messageProducer = session.createProducer(destination);
+          log.info("after session");
+          this.destination = queueTx;
+          log.info("after destination = queue");
+          this.messageProducer = session.createProducer(this.destination);
+          log.info("messageProducer created");
           this.connection.start();
-    	}
-    	catch (JMSException e) {
-    		
-    	}
-    	
+            }
+            catch (JMSException e) {
+                    log.info("jms exception");
+            }
+            catch (Exception ex) {
+                    log.info("general exception");
+            }
+            
     }
     
     public void createMessage()
     {
-    	try
-    	{
-    	  mapMessage = this.session.createMapMessage(); 
-    	  this.mapMessage.setString("emailAddress", emailMessage.emailAddress);
-    	  this.mapMessage.setString("emailCreative", emailMessage.emailCreative);
-    	  this.mapMessage.setString("subjectLine", emailMessage.subjectLine);
-    	  this.mapMessage.setString("fromAddress", emailMessage.fromAddress);
-    	  
-    	}
-    	catch (JMSException e) {
-    		
-    	}
+            log.info("jms createMessage called");
+            try
+            {
+              mapMessage = this.session.createMapMessage(); 
+              this.mapMessage.setString("emailAddress", emailMessage.emailAddress);
+              this.mapMessage.setString("emailCreative", emailMessage.emailCreative);
+              this.mapMessage.setString("subjectLine", emailMessage.subjectLine);
+              this.mapMessage.setString("fromAddress", emailMessage.fromAddress);
+              
+            }
+            catch (JMSException e) {
+                    
+            }
     }
     
     public void sendMessage()
     {
+      log.info("jms sendMessage called");
       try
       {
-    	this.messageProducer.send(this.mapMessage);
+            this.messageProducer.send(this.mapMessage);
       }
-  	  catch (JMSException e) {
-		
-  	  }      
+            catch (JMSException e) {
+                
+            }      
     }
     
-	
-	
+        public String getInfo(){
+                
+                StringBuilder sb = new StringBuilder();
+                sb.append(this.emailMessage.emailCreative);
+                sb.append("---");
+                sb.append(this.emailMessage.emailAddress);
+                
+                
+                return sb.toString();
+                
+        }
+        
 
 }
--- src/main/java/com/lambdus/emailengine/webservices/RestDefinition.java
+++ src/main/java/com/lambdus/emailengine/webservices/RestDefinition.java
@@ -22,50 +22,51 @@ import org.jboss.logging.Logger;
 @Path("/emailengine")
 public class RestDefinition {
 
-	
-	private static final Logger log = Logger.getLogger(RestDefinition.class.getName());
-	
-	@GET()
-	@Path("/sendtx")
-	@Produces("application/json")
+        
+        private static final Logger log = Logger.getLogger(RestDefinition.class.getName());
+        
+        @GET()
+        @Path("/sendtx")
+        @Produces("application/json")
     public List<String> sendTransactional(@QueryParam("email") String emailAddress, @QueryParam("templateId") int templateId, @Context UriInfo uriInfo) {
 
-		MultivaluedMap<String, String> paramsMap = uriInfo.getQueryParameters();
-		HashMap<String,String> miscParams = collectMiscParameters(paramsMap);
+                MultivaluedMap<String, String> paramsMap = uriInfo.getQueryParameters();
+                HashMap<String,String> miscParams = collectMiscParameters(paramsMap);
 
-		EmailRequest request = new EmailRequest();
-		request.setEmailAddress(emailAddress);
-		request.setTemplateId(templateId);
-		request.setTokens(miscParams);
-		
-		MessageQueueProducer mqp = new MessageQueueProducer(request);
-		mqp.initialize();
-		
-		ArrayList<String> response = new ArrayList<String>();
-		response.add("OK");
-		response.add(emailAddress);
-		response.add(String.valueOf(templateId));
-		response.add(String.valueOf(miscParams.size()));
-		
+                EmailRequest request = new EmailRequest();
+                request.setEmailAddress(emailAddress);
+                request.setTemplateId(templateId);
+                request.setTokens(miscParams);
+                
+                MessageQueueProducer mqp = new MessageQueueProducer();
+                mqp.addRequest(request);
+                mqp.initialize();
+                
+                ArrayList<String> response = new ArrayList<String>();
+                response.add("OK");
+                response.add(emailAddress);
+                response.add(String.valueOf(templateId));
+                response.add(String.valueOf(miscParams.size()));
+                response.add(mqp.getInfo());
+                
         return response;
         
 }
-	
-	private HashMap<String,String> collectMiscParameters(MultivaluedMap<String, String> paramsMap)
-	{
-		HashMap<String,String> miscParams = new HashMap<String,String>();
-		Iterator<Entry<String, List<String>>> iter = paramsMap.entrySet().iterator();
-		while (iter.hasNext()) 
-		{
-			MultivaluedMap.Entry<String,List<String>> kvp = (MultivaluedMap.Entry<String,List<String>>)iter.next(); 
-			if (kvp.getKey() != "email" && kvp.getKey() != "templateId")
-			{
-				miscParams.put( (String) kvp.getKey(), (String) kvp.getValue().get(0) );
-		    }
-		}
-		
-		return miscParams;
-	}
-	
+        
+        private HashMap<String,String> collectMiscParameters(MultivaluedMap<String, String> paramsMap)
+        {
+                HashMap<String,String> miscParams = new HashMap<String,String>();
+                Iterator<Entry<String, List<String>>> iter = paramsMap.entrySet().iterator();
+                while (iter.hasNext()) 
+                {
+                        MultivaluedMap.Entry<String,List<String>> kvp = (MultivaluedMap.Entry<String,List<String>>)iter.next(); 
+                        if (kvp.getKey() != "email" && kvp.getKey() != "templateId")
+                        {
+                                miscParams.put( (String) kvp.getKey(), (String) kvp.getValue().get(0) );
+                    }
+                }
+                
+                return miscParams;
+        }
+        
 }
-
